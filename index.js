9087598e61fa79569f66b0f70c9d082f1c072c41
Zibeke Onwabe <lizoxolo9@gmail.com>
2025-07-13 20:44:57 +0200

    improving SSL and redirecting to HTTPS

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect('https://' + req.headers.host + req.url);
+    }
+    next();
+  });
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.get('/uploads/:filename', (req, res) => {
   const filePath = path.join(__dirname, 'uploads', req.params.filename);
@@ -22,7 +29,6 @@ app.get('/uploads/:filename', (req, res) => {
     }
   });
 });
-
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -43,54 +49,47 @@ app.use('/', adminRoutes);
 app.use('/', subscribeRoutes);
 app.use('/', contactRoutes);
 
-//No login needed to this routes
+// No login needed to these routes
 app.get('/universities', (req, res) => {
   res.render('universities'); 
 });
 app.get('/news', (req, res) => {
   res.render('news');
 });
-
 app.get('/bursaries', (req, res) => {
   res.render('bursaries');
 });
-
 app.get('/management', (req, res) => {
   res.render('management');
 });
-
 app.get('/live-classes', (req, res) => {
   res.render('live-classes');
 });
 app.get('/about/students', (req, res) => {
   res.render('about/students');
 });
-
 app.get('/about/management', (req, res) => {
   res.render('about/management');
 });
-
 app.get('/about/developers', (req, res) => {
   res.render('about/developers');
 });
 app.get('/contact/developers', (req, res) => {
   res.render('contact/developers');
 });
-
 app.get('/about/teacher', (req, res) => {
   res.render('about/teachers');
 });
 
-
 app.use((req, res) => {
-    res.status(404).render('error', {
-        title: '404 - Page Not Found',
-        message: null
-    });
+  res.status(404).render('error', {
+    title: '404 - Page Not Found',
+    message: null
+  });
 });
 
 // Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
